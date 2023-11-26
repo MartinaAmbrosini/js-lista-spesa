6dc82fb9b615ff20e6ccd7c4efc4a6a761c86d8e
Martina Ambrosini <martinaambrosini17@gmail.com>
2023-11-26 16:55:10 +0100

    create: list

--- js/script.js
+++ js/script.js
@@ -6,3 +6,26 @@
 
 // - Ricordiamoci di inizializzare la variabile di contatore prima dell’inizio del ciclo while
 // - Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all’interno ciclo while.
+
+//  faccio lista spesa 
+
+const spesaArray = ["latte", "carne", "verdura", "pane", "frutta"];
+
+const spesa = document.getElementById("spesa");
+
+let text = "";
+
+// dichiaro e inizializzo il contatore 
+
+let i = 0 ;
+
+// scorro l'array con while
+
+
+while (i < spesaArray.length) { //finchè i è < della lunghezza dell'Array
+    
+   text += spesaArray[i] + "<br>";
+   i++;
+} 
+
+spesa.innerHTML= text;
